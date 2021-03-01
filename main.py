28f80d1910b2d980a7bb64e0a1274387d0f202e6
JareBear12418 <jaredgrozz@gmail.com>
2021-03-01 13:18:52 -0600

    stufff

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import yaml
 from datetime import datetime
 from git import Repo
-# pip install git-python
 
 FILE_TO_COMMIT_NAME: str = 'update_me.yaml'
 
-# read file contents to figure out how many times we commited.
-with open(FILE_TO_COMMIT_NAME, 'r') as file:
-    YAML_FILE = {
-        'UPDATE_TIMES':int(yaml.safe_load(file)['UPDATE_TIMES']) + 1,
-        'LAST_UPDATE':datetime.now().strftime("%A %B %d %Y at %X%p")
-                 }
-# Write new contents to file to be commited.
-with open(FILE_TO_COMMIT_NAME, 'w') as file:
-    yaml.dump(YAML_FILE, file, default_flow_style=False, sort_keys=True)
-repo = Repo('.')  # if repo is CWD just do '.'
-repo.index.add([FILE_TO_COMMIT_NAME])
-repo.index.commit(f'Updated {YAML_FILE["UPDATE_TIMES"]} times')
-origin = repo.remote('origin')
-origin.push()
+def update_file_to_commit():
+    # read file contents to figure out how many times we commited.
+    with open(FILE_TO_COMMIT_NAME, 'r') as file:
+        YAML_FILE = {
+            'UPDATE_TIMES':int(yaml.safe_load(file)['UPDATE_TIMES']) + 1,
+            'LAST_UPDATE':datetime.now().strftime("%A %B %d %Y at %X%p")
+            }
+    # Write new contents to file to be commited.
+    with open(FILE_TO_COMMIT_NAME, 'w') as file: yaml.dump(YAML_FILE, file, default_flow_style=False, sort_keys=True)
+    return YAML_FILE
+
+def commit_repository(YAML_FILE):
+    repo = Repo('.')  # if repo is CWD just do '.'
+    repo.index.add([FILE_TO_COMMIT_NAME])
+    repo.index.commit(f'Updated {YAML_FILE["UPDATE_TIMES"]} times on {YAML_FILE["UPDATE_TIMES"]}.')
+    origin = repo.remote('origin')
+    origin.push()
+
+if __name__ == '__main__': 
+    commit_repository(update_file_to_commit())
